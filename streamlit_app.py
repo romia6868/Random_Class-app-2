4189f95f20372aff4c9d66aa5239e216a70043e0
romia6868 <romia6868@gmail.com>
2026-03-13 00:46:47 +0200

    Refactor image processing and enhance resizing logic

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,6 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 ZIP_PATH = os.path.join(BASE_DIR, "My_Classmates_small.zip")
 EXTRACT_PATH = os.path.join(BASE_DIR, "My_Classmates")
 
-# חילוץ ה-ZIP אם עוד לא חולץ
 if not os.path.exists(EXTRACT_PATH):
     with zipfile.ZipFile(ZIP_PATH, "r") as zip_ref:
         zip_ref.extractall(EXTRACT_PATH)
@@ -26,43 +25,58 @@ def generate_class_image():
         st.error("Background image not found")
         st.stop()
 
-    bg = cv2.resize(bg, (900, 600))
-
-    if not os.path.exists(REFERENCE_DIR):
-        st.error("Students folder not found")
-        st.stop()
+    bg = cv2.resize(bg, (900, 600), interpolation=cv2.INTER_CUBIC)
 
     students = os.listdir(REFERENCE_DIR)
 
     num_present = random.randint(0, len(students))
     present = random.sample(students, num_present)
 
+    rows = 2
+    cols = 5
+
+    cell_w = bg.shape[1] // cols
+    cell_h = bg.shape[0] // rows
+
+    positions = []
+
+    for r in range(rows):
+        for c in range(cols):
+            x = c * cell_w
+            y = r * cell_h
+            positions.append((x, y))
+
+    random.shuffle(positions)
+
+    i = 0
+
     for name in present:
 
-        student_dir = os.path.join(REFERENCE_DIR, name)
-        imgs = os.listdir(student_dir)
+        if i < len(positions):
 
-        if len(imgs) > 0:
+            student_dir = os.path.join(REFERENCE_DIR, name)
+            imgs = os.listdir(student_dir)
 
-            img_path = os.path.join(student_dir, random.choice(imgs))
-            face = cv2.imread(img_path)
+            if len(imgs) > 0:
 
-            if face is not None:
+                img_path = os.path.join(student_dir, random.choice(imgs))
+                face = cv2.imread(img_path)
 
-                h, w, c = face.shape
+                if face is not None:
 
-                # הגדלה פי 3
-                new_w = int(w * 3)
-                new_h = int(h * 3)
+                    new_w = int(cell_w * 0.8)
+                    new_h = int(cell_h * 0.8)
 
-                face = cv2.resize(face, (new_w, new_h))
+                    face = cv2.resize(face, (new_w, new_h), interpolation=cv2.INTER_CUBIC)
 
-                if new_h < bg.shape[0] and new_w < bg.shape[1]:
+                    x, y = positions[i]
 
-                    x = random.randint(0, bg.shape[1] - new_w)
-                    y = random.randint(0, bg.shape[0] - new_h)
+                    x = x + (cell_w - new_w) // 2
+                    y = y + (cell_h - new_h) // 2
 
                     bg[y:y+new_h, x:x+new_w] = face
+
+                    i = i + 1
 
     return bg, present
 
